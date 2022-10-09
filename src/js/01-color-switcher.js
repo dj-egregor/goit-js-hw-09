2d58b0edde07b3c4b96e06e092d99ee9037e96cb
Alexander Kushnir <dj.egregor1@gmail.com>
2022-10-09 20:34:52 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let timerId = null;
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+stoptBtn.disabled = true;
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function switchButtonsDisable(param) {
+  stoptBtn.disabled = param ? false : true;
+  startBtn.disabled = param ? true : false;
+}
+
+startBtn.addEventListener('click', () => {
+  switchButtonsDisable(true);
+  changeColor();
+  timerId = setInterval(changeColor, 1000);
+});
+
+stoptBtn.addEventListener('click', () => {
+  switchButtonsDisable(false);
+  clearInterval(timerId);
+});
